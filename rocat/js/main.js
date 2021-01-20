6577ee6303f203df38d817e5a8c4f29fb0ca3530
Vitaly <bakuntsev@gmail.com>
2021-01-20 21:05:31 +0300

    Добавление техники и услуг

---
+++ rocat/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.partners__slider').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+    });
+  });
