32d68a1daa370fac822b8e8af969e385e162d75d
Vitaly <bakuntsev@gmail.com>
2021-04-11 16:14:01 +0300

    update 0.3

---
+++ straw/js/app.js
@@ -0,0 +1,60 @@
+const html = document.documentElement;
+const canvas = document.getElementById("hero-section");
+const context = canvas.getContext("2d");
+
+const frameCount = 170;
+const currentFrame = index => (
+  `canvas/jpg/${index.toString().padStart(4, '0')}.jpg`
+)
+
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+  }
+};
+
+const img = new Image()
+img.src = currentFrame(1);
+canvas.width=868;
+canvas.height=434;
+img.onload=function(){
+  context.drawImage(img, 0, 0);
+}
+
+const updateImage = index => {
+  img.src = currentFrame(index);
+  context.drawImage(img, 0, 0);
+}
+
+window.addEventListener('scroll', () => {  
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop / maxScrollTop;
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.ceil(scrollFraction * frameCount)
+  );
+  
+  requestAnimationFrame(() => updateImage(frameIndex + 1))
+});
+
+preloadImages()
+
+// var controller = new ScrollMagic.Controller();
+
+var controller = new ScrollMagic.Controller({
+  globalSceneOptions: {
+      triggerHook: 'onLeave'
+  }
+});
+
+$(function () { // wait for document ready
+  // build scene
+  var scene = new ScrollMagic.Scene({
+                                      triggerElement: "#trigger", 
+                                      duration: 4000
+                                    })
+                             .setPin("#pin")
+                             .addTo(controller);
+});       
