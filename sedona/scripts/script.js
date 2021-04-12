b075d93093f8f2e7ee099c653c31e4d435f4c40f
Vitaly <bakuntsev@gmail.com>
2021-04-12 21:22:36 +0300

    add practic

---
+++ sedona/scripts/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.navigation__list'),
+    menuItem = document.querySelectorAll('.navigation__item'),
+    hamburger = document.querySelector('.navigation__hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('navigation__hamburger--active');
+        menu.classList.toggle('navigation__list--active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('navigation__hamburger--active');
+            menu.classList.toggle('navigation__list--active');
+        })
+    })
+})
